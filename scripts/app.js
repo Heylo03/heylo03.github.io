72eaa7c53486cdc144264b42ab37727a8f687500
Pablo3 <pcorralr3@gmail.com>
2025-02-18 09:15:07 +0100

    avance

--- scripts/app.js
+++
@@ -1,7 +0,0 @@
-// This file contains the JavaScript code for the website. It handles interactivity and dynamic content on the webpage.
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Document is ready!');
-    
-    // Add your interactivity code here
-});
